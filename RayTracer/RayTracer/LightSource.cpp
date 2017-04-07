88169a4aef393cd3c35d99af3d84ed42a34072f6
Dan O'Connor <dpo3ys@virginia.edu>
2017-04-06 22:46:05 -0700

    Fixed point light positioning.

--- RayTracer/RayTracer/LightSource.cpp
+++ RayTracer/RayTracer/LightSource.cpp
@@ -5,17 +5,14 @@ namespace RayTracer
 {
 	LightSource::LightSource(Vector posOrDir)
 	{
+		// TODO: posOrDir used to be normalized, but that broke pointlights (moved their position). Should this be normalized for suns?
 		m_posOrDir = posOrDir;
-		//m_posOrDir.normalize();
-
 		m_color = RGB(0xff, 0xff, 0xff);
 	}
 
 	LightSource::LightSource(Vector posOrDir, COLORREF color)
 	{
 		m_posOrDir = posOrDir;
-		m_posOrDir.normalize();
-
 		m_color = color;
 	}
 }
